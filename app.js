95f7a500409f266912166e0eced5dcd6645a204b
unknown <shivamk898@gmail.com>
2019-08-12 14:47:16 +0530

    adding filess

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'LightBlog', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('client/dist'));
+	app.use(express.static('client/build'));
 }
 
 // Add models
